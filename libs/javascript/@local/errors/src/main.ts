8f60a78b40a62babac275f1fc85b54558cdaa00b
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-15 15:38:02 +0000

    Add js analogue

---
+++ libs/javascript/@local/errors/src/main.ts
@@ -0,0 +1,13 @@
+import { ErrorCode } from "./error-code";
+
+export { type ErrorCode } from "./error-code";
+
+/**
+ * Defines a logical error model that is suitable for different programming environments, including REST APIs and RPC
+ * APIs.
+ */
+export type Status<D> = {
+  code: ErrorCode;
+  message: string;
+  details: D;
+};
